3cd327bd2035c5a4579bb8fa0be7b7d28ad75bd9
JungWoo <wjddnwh12@naver.com>
2022-06-07 14:02:30 +0900

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,113 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () =>{
+//   menu.classList.toggle('fa-times');
+//   navbar.classList.toggle('active');
+// }
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+// document.querySelector('#search-icon').onclick = () =>{
+//   document.querySelector('#search-form').classList.toggle('active');
+// }
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
+
+
+const loginId = document.getElementById('loginId');
+const loginPw = document.getElementById('loginPassword');
+const loginBtn = document.getElementById('login_btn');
+
+function color() {
+    if((loginId.value.length>0 && loginId.value.indexOf("@")!==-1) 
+        && loginPassword.value.length>=5){
+        login_btn.style.backgroundColor = "#0095F6";
+        loginBtn.disabled = false;
+    }else{
+        loginBtn.style.backgroundColor = "#C0DFFD";
+        loginBtn.disabled = true;
+    }
+}
+
+function moveToMain(){
+    location.replace("./main.html");
+}
+
+loginId.addEventListener('keyup', color);
+loginPw.addEventListener('keyup', color);
+loginBtn.addEventListener('click',moveToMain);
